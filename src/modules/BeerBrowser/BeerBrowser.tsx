938fad347d9745f9d4c92bedefda43fad81f870f
Marcin Grzmil <marcin.grzmil@hotmail.com>
2023-06-22 19:43:08 +0200

    added pagination component

--- src/modules/BeerBrowser/BeerBrowser.tsx
+++ src/modules/BeerBrowser/BeerBrowser.tsx
@@ -6,22 +6,31 @@ import { httpClient } from "../../common";
 import styles from "./beerBrowser.module.css";
 import { beerDataMapper } from "./beerDataMapper";
 import { BeerType } from "../../types";
+import { CustomPagination } from "../../components/CustomPagination";
 
 export const BeerBrowser: FC = () => {
+  const [page, setPage] = useState(1);
+  const setPageHandler = (page: number) => {
+    setPage(page);
+  };
+
   const [beerData, setBeerData] = useState<BeerType[]>();
   useEffect(() => {
-    httpClient.get("/beers").then((response: AxiosResponse) => {
+    httpClient.get(`/beers?page=${page}&per_page=9`).then((response: AxiosResponse) => {
       setBeerData(beerDataMapper(response.data));
     });
-    console.log(beerData);
-  }, []);
+  }, [page]);
+
   return (
     <>
-      <Container className={styles.beerCardsContainer}>
-        {beerData?.map((beer) => {
-          return <BeerCard name={beer.name} tagline={beer.tagline} image={beer.imageUrl} key={beer.id} />;
-        })}
-      </Container>
+      <div>
+        <Container className={styles.beerCardsContainer}>
+          {beerData?.map((beer) => {
+            return <BeerCard name={beer.name} tagline={beer.tagline} image={beer.imageUrl} key={beer.id} />;
+          })}
+        </Container>
+        <CustomPagination page={page} setPageHandler={setPageHandler} />
+      </div>
     </>
   );
 };
