4361c9971e1ed4fbb2079bfaf612868b6c0afa04
g-marcin <marcin.grzmil@hotmail.com>
2023-06-25 13:43:21 +0200

    added async image component

--- src/modules/BeerBrowser/BeerCard/BeerCard.tsx
+++ src/modules/BeerBrowser/BeerCard/BeerCard.tsx
@@ -1,29 +1,37 @@
-import { FC, useContext } from 'react';
+import { FC } from 'react';
 import { Card } from 'react-bootstrap';
 import { useNavigate } from 'react-router-dom';
-import { ThemeContext } from '../../../contexts';
+import { Loader } from '../../../components';
+import { AsyncImage } from '../../../components/AsyncImage';
+import { BeerShadow } from '../../../components/BeerShadow';
 import styles from './beerCard.module.css';
 
 type BeerCardProps = {
-  image?: string;
-  name?: string;
-  tagline?: string;
+  image: string;
+  name: string;
+  tagline: string;
   id: number;
+  isLoading: boolean;
 };
 
-export const BeerCard: FC<BeerCardProps> = ({ image, name, tagline, id }) => {
+export const BeerCard: FC<BeerCardProps> = ({ image, name, tagline, id, isLoading }) => {
   const navigate = useNavigate();
   const beerCardHandler = (id: number) => {
     navigate(`/details/${id}`);
   };
-  const {isDark} = useContext(ThemeContext)
   return (
     <Card className={styles.beerCard} onClick={() => beerCardHandler(id)}>
-      <div className={styles["image-wrapper"]}>
-      <Card.Img variant="top" src={image} className={styles.cardImage} />
-<div className={isDark? styles["image-shadow-dark"]:styles["image-shadow-light"]}></div>
+      <div className={styles['image-wrapper']}>
+        {isLoading ? (
+          <Loader />
+        ) : (
+          <>
+            <AsyncImage src={image} alt="bottle image" className={styles.cardImage} />
+            <BeerShadow variant="card" />
+          </>
+        )}
       </div>
-      
+
       <Card.Body className={styles.cardBody}>
         <Card.Title>{name}</Card.Title>
         <Card.Text>{tagline}</Card.Text>
--- src/modules/BeerDetails/BeerDetails.tsx
+++ src/modules/BeerDetails/BeerDetails.tsx
@@ -1,17 +1,16 @@
-import { FC, useContext } from 'react';
-import { Card } from 'react-bootstrap';
+import { FC } from 'react';
 import { useParams } from 'react-router-dom';
 import { Loader } from '../../components';
-import { ThemeContext } from '../../contexts';
+import { AsyncImage } from '../../components/AsyncImage';
+import { BeerShadow } from '../../components/BeerShadow';
 import { useBeerDetails } from '../../hooks/useBeerDetails';
 import { BeerInfo } from './BeerInfo';
 import { IngredientsInfo } from './IngredientsInfo';
 import styles from './beerDetails.module.css';
 
 const BeerDetails: FC = () => {
-  const { id} = useParams();
+  const { id } = useParams();
   const { beerDetails, isLoading } = useBeerDetails(id);
-  const{isDark} = useContext(ThemeContext)
 
   if (!beerDetails) {
     return;
@@ -23,13 +22,11 @@ const BeerDetails: FC = () => {
         <Loader />
       ) : (
         <div className={`${styles.wrapper} `}>
-     
           <BeerInfo beerDetails={beerDetails} />
-          
-         
-          <div className={styles["image-wrapper"]}>
-          <Card.Img variant="top" src={beerDetails?.imageUrl} className={styles.detailsImage} />
-<div className={isDark? styles["image-shadow-dark"]:styles["image-shadow-light"]}></div>
+          <div className={styles['image-wrapper']}>
+            <AsyncImage src={beerDetails.imageUrl} alt="bottle image" className={styles.detailsImage} />
+
+            <BeerShadow variant="details" />
           </div>
           <IngredientsInfo beerDetails={beerDetails} />
         </div>
